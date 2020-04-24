dfec5e2bd29d5b8a1181bf2d1311f45dce2102cc
AMANDA KOMSKY <amandakomsky@AMANDAs-MacBook-Pro.local>
2020-04-24 15:23:43 -0400

    Project hand-tracking-drawing initial commit

---
+++ Hand_Tracking_drawing/Assets/Scripts/Painting.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Painting : MonoBehaviour
+{
+    public GameObject Ink;
+    public GameObject Marker;
+    // public float InkSize = 0.1f;
+    // public Controller controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    void Update() {
+
+      if (OVRInput.Get(OVRInput.Button.One)) {
+        Vector3 bk = Marker.transform.TransformDirection(Vector3.back);
+        RaycastHit hit;
+        if (Physics.Raycast(Marker.transform.position, bk, .1f)){
+          Marker.SetActive(false);
+        }
+      }
+
+      if (OVRInput.Get(OVRInput.Button.Two)){
+        Marker.SetActive(true);
+      }
+    }
+
+
+}
+    // void FixedUpdate()
+    // {
+    //     Vector3 fwd = transform.TransformDirection(Vector3.forward);
+    //     if (OVRInput.Get(OVRInput.Button.One)){
+    //       if (Physics.Raycast(Marker.transform.position, fwd, 2)){
+    //         print("There is something in front of the object!");
+    //         Debug.Log("HIT");
+    //         var mark = Instantiate(Ink, Marker.transform.position, Marker.transform.rotation);
+    //       }
+    //     }
+    //
+    // }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+    //   if (OVRInput.Get(OVRInput.Button.One)){
+    //     Debug.log("PRESSED");
+    //     // RaycastHit hit;
+    //     // if (Physics.Raycast(Marker.transform.position, transform.TransformDirection(Vector3.forward), out hit, 10)) {
+    //     //   Debug.Log("Hit");
+    //     //   var mark = Instantiate(Ink, hit.transform.position, Marker.transform.rotation);
+    //     // }
+    //   }
+    // }
+    //   if (OVRInput.Get(OVRInput.Button.One)){
+    //     Vector3 pen_position = Marker.transform.position + Vector3.down * 0.1f;
+    //     var mark = Instantiate(Ink, pen_position, Marker.transform.rotation);
+    //   }
+    //   OnCollisionEnter();
+    //   // else{
+    //   //   if (OVRInput.Get(OVRInput.Button.One && Marker.transform.position);
+    //   // }
+    //   // if (OVRInput.Get(OVRInput.Button.One)) {
+    //   //   var Ray = Camera.main.ScreenPointToRay(OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch));
+    //   //   RaycastHit hit;
+    //   //   if(Physics.Raycast(Ray, out hit)) {
+    //   //     var go = Instantiate(Marker, hit.point + Vector3.up * 0.1f, Quaternion.identity, transform);
+    //   //     go.transform.localScale = Vector3.one * InkSize;
+    //   //   }
+    //   // }
+    // }
+    // void OnCollisionEnter(Collision collision)
+    // {
+    //     //Check for a match with the specific tag on any GameObject that collides with your GameObject
+    //     if (collision.gameObject.tag == "Board" )
+    //     {
+    //       Vector3 pen_position = Marker.transform.position + Vector3.down * 0.1f;
+    //       var mark = Instantiate(Ink, pen_position, Marker.transform.rotation);
+    //         //If the GameObject has the same tag as specified, output this message in the console
+    //         // Vector3 pen_position = Marker.transform.position + Vector3.down * 0.1f;
+    //         // var mark = Instantiate(Ink, collision.gameObject.transform.position, Marker.transform.rotation);
+    //     }
+    // }
+// }
---
+++ Hand_Tracking_drawing/Assets/Scripts/grab_object.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using OVRHand;
+
+public class grab_object : MonoBehaviour
+{
+    // public GameObject cylinder
+    public GameObject item;
+    public GameObject tip;
+    private OVRHand l_hand;
+    private OVRSkeleton l_finger;
+    public OVRHand r_hand;
+    public OVRSkeleton r_finger;
+
+    private bool r_isIndexFingerPinching;
+    private bool l_isIndexFingerPinching;
+    private bool l_isMiddleFingerPinching;
+    private Vector3 initial_position;
+    public GameObject ink;
+    public GameObject ink2;
+
+    private Renderer inkRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        l_hand = GetComponent<OVRHand>();
+        l_finger = GetComponent<OVRSkeleton>();
+        r_finger = r_hand.GetComponent<OVRSkeleton>();
+        r_isIndexFingerPinching = r_hand.GetFingerIsPinching(OVRHand.HandFinger.Index);
+        l_isIndexFingerPinching = l_hand.GetFingerIsPinching(OVRHand.HandFinger.Index);
+
+        initial_position = item.transform.position;
+        inkRenderer = ink.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      isGrabbing();
+      colorChange();
+    }
+
+    void isGrabbing() {
+      // Ray ray;
+      Vector3 current = item.transform.position;
+      // Experiment with left and right hand pinch
+      l_isIndexFingerPinching = l_hand.GetFingerIsPinching(OVRHand.HandFinger.Index);
+      r_isIndexFingerPinching = r_hand.GetFingerIsPinching(OVRHand.HandFinger.Index);
+
+      if (r_isIndexFingerPinching) {
+        item.transform.position = Vector3.Lerp(item.transform.position, r_finger.Bones[20].Transform.position, 50f*Time.deltaTime);
+        item.transform.rotation = Quaternion.Slerp(item.transform.rotation, r_hand.transform.rotation, 10f*Time.deltaTime);
+      }
+      if (!r_isIndexFingerPinching) {
+        item.transform.position = Vector3.Lerp(item.transform.position, initial_position, 50f*Time.deltaTime);
+        item.transform.rotation = Quaternion.Slerp(item.transform.rotation, r_hand.transform.rotation, 10f*Time.deltaTime);
+      }
+
+      if (r_isIndexFingerPinching && l_isIndexFingerPinching){
+        RaycastHit hit;
+        if (Physics.Raycast(tip.transform.position, -tip.transform.up, out hit, .2f)){
+          if(hit.collider.tag == "Board"){
+            Instantiate(ink, hit.point-Vector3.forward*0.01f, hit.transform.rotation);
+          }
+        }
+      }
+    }
+
+    void colorChange() {
+      l_isMiddleFingerPinching = l_hand.GetFingerIsPinching(OVRHand.HandFinger.Middle);
+      if (l_isMiddleFingerPinching) {
+        inkRenderer.material.SetColor("_Color", Color.red);
+      }
+    }
+
+    // if (r_isIndexFingerPinching) {
+      // item.transform.rotation = r_hand.transform.rotation;
+      // item.transform.position = r_hand.transform.position;
+      // item.transform.position = Vector3.Lerp(current, new Vector3(current.x,r_hand.transform.position.y,r_hand.transform.position.z), Time.deltaTime);
+    // }
+
+      // if (!l_isIndexFingerPinching){
+      //   item.transform.position = Vector3.MoveTowards(item.transform.position, initial_position, 1f *Time.deltaTime );
+      //   item.rotation = new Vector3(0,0,0);
+      //   // item.transform.rotation = Quaternion.RotateTowards(new Vector3(0,0,0), Quaternion.Euler(l_hand.transform.rotation), 1f*Time.deltaTime);
+      //   // item.transform.rotation = Vector3.RotateTowards(new Vector3(0,0,0), l_hand.transform.rotation, 1f*Time.deltaTime, 0.0f);
+      // }
+      // if(l_isIndexFingerPinching ){
+      //   item.transform.position = Vector3.MoveTowards(item.transform.position, r_hand.transform.position + Vector3.forward*.1f, 1f *Time.deltaTime );      }
+      //   // item.transform.rotation = Vector3.RotateTowards(l_hand.transform.rotation, new Vector3(0,0,0), 1f*Time.deltaTime, 0.0f);
+      //   // item.transform.rotation = Vec.RotateTowards(Quaternion.Euler(l_hand.transform.rotation), new Vector3(0,0,0), 1f*Time.deltaTime);
+    // }
+    //
+    //   Vector3 placement = r_finger.Bones[20].Transform.position + Vector3.left*.2f;
+    //   // Vector3 placement = r_hand.transform.position;
+
+}
